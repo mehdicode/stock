dde7ef787393af4102678b238d94387a8eaf2602
mehdicode <kalan.mahdi@yahoo.com>
2018-03-23 23:36:16 -0700

    manage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
