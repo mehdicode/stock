f06256437c31144a8481ca866bf4d8988878e1ab
mehdicode <kalan.mahdi@yahoo.com>
2018-03-23 23:43:11 -0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ManageComponent } from './components/manage/manage.component';
+import { AppRoutes } from './app.routes';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { ManageComponent } from './components/manage/manage.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutes
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
